00f7b88e8a71c56af983cd807d8083ca122a780c
gzhai5 <gzhai5@illinois.edu>
2025-07-16 15:53:51 -0400

    show UI of this contact's chat history

--- src/components/sms/ContactDetail.tsx
+++ src/components/sms/ContactDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { format } from 'date-fns';
 import { Contact, Message } from '../../types/sms';
 
